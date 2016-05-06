5f1ee970a20b8984f135d878c8433bb758606f67
Ryan <rnewsom@msudenver.edu>
2016-05-05 23:53:54 -0600

    Removed bad commits, the last 2 to be eact

---
+++ src/ex17/RyanNewsomKyleFrisbie/MainTest.java
@@ -0,0 +1,14 @@
+package ex17.RyanNewsomKyleFrisbie;
+
+import org.junit.Test;
+
+/**
+ * Created by Ryan on 5/5/2016.
+ */
+public class MainTest {
+
+    @Test
+    public void testTakeInUserInput() throws Exception {
+
+    }
+}
